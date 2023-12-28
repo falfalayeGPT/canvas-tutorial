f5582e1dea9c36ac89ba8d08a7a538cd8cef4bee
Sanusi Hassan <sanusihassan122@gmail.com>
2023-12-28 16:39:20 +0000

    packaged

--- canvas-resize/index.js
+++ canvas-resize/index.js
@@ -3,26 +3,39 @@ if (canvas) {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     var c = canvas === null || canvas === void 0 ? void 0 : canvas.getContext("2d");
-    c.fillStyle = "blue";
-    // c?.fillRect(100, 100, 100, 100);
-    // for (let i = 0; i < 10; i++) {
-    //   c?.fillRect(100 * i, 100 * i, 100, 100);
+    // c!.fillStyle = "blue";
+    // // c?.fillRect(100, 100, 100, 100);
+    // // for (let i = 0; i < 10; i++) {
+    // //   c?.fillRect(100 * i, 100 * i, 100, 100);
+    // // }
+    // c?.beginPath();
+    // c?.moveTo(100, 100);
+    // c?.lineTo(300, 400);
+    // c?.lineTo(400, 500);
+    // c!.strokeStyle = "red";
+    // // c?.stroke();
+    // for (let i = 1; i < 500; i += 1) {
+    //   c?.beginPath();
+    //   // c!.fillStyle = "rgba(255, 0, 0, .5)";
+    //   let x = Math.random() * window.innerWidth;
+    //   let y = Math.random() * window.innerHeight;
+    //   let o = Math.random() * 1;
+    //   c!.fillStyle = `rgba(${(i * 100 + x - y) % 255}, ${(i * 10 + x) % 255}, ${
+    //     (i * 5 + y) % 255
+    //   }, ${o})`;
+    //   c?.fill();
     // }
-    c === null || c === void 0 ? void 0 : c.beginPath();
-    c === null || c === void 0 ? void 0 : c.moveTo(100, 100);
-    c === null || c === void 0 ? void 0 : c.lineTo(300, 400);
-    c === null || c === void 0 ? void 0 : c.lineTo(400, 500);
-    c.strokeStyle = "red";
-    // c?.stroke();
-    for (var i = 1; i < 500; i += 1) {
-        c === null || c === void 0 ? void 0 : c.beginPath();
-        // c!.fillStyle = "rgba(255, 0, 0, .5)";
-        var x = Math.random() * window.innerWidth;
-        var y = Math.random() * window.innerHeight;
-        var o = Math.random() * 1;
-        c.fillStyle = "rgba(".concat((i * 100 + x - y) % 255, ", ").concat((i * 10 + x) % 255, ", ").concat((i * 5 + y) % 255, ", ").concat(o, ")");
-        c === null || c === void 0 ? void 0 : c.arc(x, y, 30, 0, 2 * Math.PI * 2);
-        c === null || c === void 0 ? void 0 : c.fill();
-    }
+    animate(c);
 }
-// c?.strokeStyle()
+var x = 200;
+function animate(c) {
+    if (x === 400) {
+        return;
+    }
+    c === null || c === void 0 ? void 0 : c.clearRect(0, 0, innerWidth, innerHeight);
+    c === null || c === void 0 ? void 0 : c.beginPath();
+    c === null || c === void 0 ? void 0 : c.arc(x, 200, 30, 0, 2 * Math.PI * 2);
+    c === null || c === void 0 ? void 0 : c.stroke();
+    x += 1;
+    requestAnimationFrame(function () { return animate(c); });
+}
--- canvas-resize/index.ts
+++ canvas-resize/index.ts
@@ -4,28 +4,39 @@ if (canvas) {
   canvas.height = window.innerHeight;
 
   const c = canvas?.getContext("2d");
-  c!.fillStyle = "blue";
-  // c?.fillRect(100, 100, 100, 100);
-  // for (let i = 0; i < 10; i++) {
-  //   c?.fillRect(100 * i, 100 * i, 100, 100);
+  // c!.fillStyle = "blue";
+  // // c?.fillRect(100, 100, 100, 100);
+  // // for (let i = 0; i < 10; i++) {
+  // //   c?.fillRect(100 * i, 100 * i, 100, 100);
+  // // }
+  // c?.beginPath();
+  // c?.moveTo(100, 100);
+  // c?.lineTo(300, 400);
+  // c?.lineTo(400, 500);
+  // c!.strokeStyle = "red";
+  // // c?.stroke();
+  // for (let i = 1; i < 500; i += 1) {
+  //   c?.beginPath();
+  //   // c!.fillStyle = "rgba(255, 0, 0, .5)";
+  //   let x = Math.random() * window.innerWidth;
+  //   let y = Math.random() * window.innerHeight;
+  //   let o = Math.random() * 1;
+  //   c!.fillStyle = `rgba(${(i * 100 + x - y) % 255}, ${(i * 10 + x) % 255}, ${
+  //     (i * 5 + y) % 255
+  //   }, ${o})`;
+  //   c?.fill();
   // }
-  c?.beginPath();
-  c?.moveTo(100, 100);
-  c?.lineTo(300, 400);
-  c?.lineTo(400, 500);
-  c!.strokeStyle = "red";
-  // c?.stroke();
-  for (let i = 1; i < 500; i += 1) {
-    c?.beginPath();
-    // c!.fillStyle = "rgba(255, 0, 0, .5)";
-    let x = Math.random() * window.innerWidth;
-    let y = Math.random() * window.innerHeight;
-    let o = Math.random() * 1;
-    c!.fillStyle = `rgba(${(i * 100 + x - y) % 255}, ${(i * 10 + x) % 255}, ${
-      (i * 5 + y) % 255
-    }, ${o})`;
-    c?.arc(x, y, 30, 0, 2 * Math.PI * 2);
-    c?.fill();
-  }
+  animate(c);
 }
-// c?.strokeStyle()
+let x = 200;
+function animate(c: CanvasRenderingContext2D | null) {
+  if (x === 400) {
+    return;
+  }
+  c?.clearRect(0, 0, innerWidth, innerHeight);
+  c?.beginPath();
+  c?.arc(x, 200, 30, 0, 2 * Math.PI * 2);
+  c?.stroke();
+  x += 1;
+  requestAnimationFrame(() => animate(c));
+}
